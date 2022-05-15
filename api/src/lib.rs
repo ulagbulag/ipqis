10dd3a30c8e366d5b2d9efbf4655e2d69ea58eb7
Ho Kim <ho.kim@ulagbulag.io>
2022-05-15 15:58:28 +0000

    Add some frame codes

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,8 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub extern crate ipqis_common as common;
+
+#[cfg(feature = "ulagbulag")]
+pub use ipqis_api_ulagbulag::*;
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,8 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use bytecheck::CheckBytes;
+use ipiis_api::client::IpiisClient;
+use ipis::{async_trait::async_trait, core::account::GuaranteeSigned};
+use rkyv::{Archive, Deserialize, Serialize};
+
+#[async_trait]
+pub trait Ipqis {}
+
+#[async_trait]
+impl Ipqis for IpiisClient {}
+
+pub type Request = GuaranteeSigned<RequestType>;
+
+#[derive(Clone, Debug, PartialEq, Archive, Serialize, Deserialize)]
+#[archive(compare(PartialEq))]
+#[archive_attr(derive(CheckBytes, Debug, PartialEq))]
+pub enum RequestType {
+    TODO,
+}
+
+#[derive(Clone, Debug, PartialEq, Archive, Serialize, Deserialize)]
+#[archive(compare(PartialEq))]
+#[archive_attr(derive(CheckBytes, Debug, PartialEq))]
+pub enum Response {
+    TODO,
 }
