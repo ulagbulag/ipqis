7ea147a6b46552c7f7d27defd9ef6db863610e36
Ho Kim <ho.kim@ulagbulag.io>
2022-05-18 17:30:09 +0000

    Add `search_functions` for Ipqis

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,13 +1,44 @@
+#![feature(more_qualified_paths)]
+
 use bytecheck::CheckBytes;
-use ipiis_common::Ipiis;
-use ipis::{async_trait::async_trait, core::account::GuaranteeSigned};
+use ipiis_common::{external_call, Ipiis};
+use ipis::{
+    async_trait::async_trait,
+    core::{account::GuaranteeSigned, anyhow::Result},
+    function::{DynFunction, Function},
+};
 use rkyv::{Archive, Deserialize, Serialize};
 
 #[async_trait]
-pub trait Ipqis {}
+pub trait Ipqis {
+    async fn search_functions(&self, query: DynFunction) -> Result<Vec<Function>>;
+}
 
 #[async_trait]
-impl<IpiisClient> Ipqis for IpiisClient where IpiisClient: Ipiis + Send + Sync {}
+impl<IpiisClient> Ipqis for IpiisClient
+where
+    IpiisClient: Ipiis + Send + Sync,
+{
+    async fn search_functions(&self, query: DynFunction) -> Result<Vec<Function>> {
+        // next target
+        let target = self.get_account_primary(KIND.as_ref()).await?;
+
+        // pack request
+        let req = RequestType::SearchFunctions { query };
+
+        // external call
+        let (functions,) = external_call!(
+            call: self
+                .call_permanent_deserialized(&target, req)
+                .await?,
+            response: Response => SearchFunctions,
+            items: { functions },
+        );
+
+        // unpack response
+        Ok(functions)
+    }
+}
 
 pub type Request = GuaranteeSigned<RequestType>;
 
@@ -15,14 +46,14 @@ pub type Request = GuaranteeSigned<RequestType>;
 #[archive(compare(PartialEq))]
 #[archive_attr(derive(CheckBytes, Debug, PartialEq))]
 pub enum RequestType {
-    TODO,
+    SearchFunctions { query: DynFunction },
 }
 
 #[derive(Clone, Debug, PartialEq, Archive, Serialize, Deserialize)]
 #[archive(compare(PartialEq))]
 #[archive_attr(derive(CheckBytes, Debug, PartialEq))]
 pub enum Response {
-    TODO,
+    SearchFunctions { functions: Vec<Function> },
 }
 
 ::ipis::lazy_static::lazy_static! {
